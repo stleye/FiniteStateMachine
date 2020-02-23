6ecefbbc15d918c1eb1a7d28d012b600c6024fd1
Sebastian Tleye <stleye@ovloop.com>
2020-02-23 13:31:17 -0300

    Initial Commit

---
+++ FiniteStateMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FiniteStateMachine
+//
+//  Created by Sebastian Tleye on 23/02/2020.
+//  Copyright © 2020 HumileAnts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
