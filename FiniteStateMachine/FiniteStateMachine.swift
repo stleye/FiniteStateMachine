0fe6ee9af669225389fed364bb2e85bc6f070ddb
Sebastian Tleye <stleye@ovloop.com>
2020-02-29 19:54:56 -0300

    Added actions to transitions Added conditions to transitions Added timer to the FSM

--- FiniteStateMachine/FiniteStateMachine.swift
+++ FiniteStateMachine/FiniteStateMachine.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct FiniteStateMachine {
+class FiniteStateMachine {
 
     struct State: Hashable {
         private(set) var name: String
@@ -27,23 +27,37 @@ struct FiniteStateMachine {
     }
 
     struct Transition: Hashable {
+
+        var hashValue: Int {
+            return origin.hashValue ^ input.hashValue ^ destination.hashValue
+        }
+
+        static func == (lhs: FiniteStateMachine.Transition, rhs: FiniteStateMachine.Transition) -> Bool {
+            return lhs.origin == rhs.origin && lhs.input == rhs.input && lhs.destination == rhs.destination
+        }
+
         var origin: State
         var input: Symbol
         var destination: State
+        var condition: ((FiniteStateMachine) -> Bool) = { _ in true }
+        var action: ((FiniteStateMachine) -> Void) = { _ in }
     }
 
     typealias Symbol = String
     typealias Event = (Transition) -> Void
 
+    var variables: [String: Any] = [:]
+
+    private(set) var timer: Int
     private(set) var currentState: State
     private(set) var initialState: State
 
     private var states: Set<State>
     private var symbols: Set<Symbol>
     private var transitions: [Transition]
-    private var events: [Transition: Event]
+    private var internalTimer: Timer?
 
-    init(initialState: State, transitions: [Transition]) {
+    init(initialState: State, transitions: [Transition], variables: [String: Any] = [:]) {
         self.initialState = initialState
         self.states = [initialState]
         self.symbols = []
@@ -54,21 +68,18 @@ struct FiniteStateMachine {
         }
         self.transitions = transitions
         self.currentState = initialState
-        self.events = [:]
+        self.variables = variables
+        self.timer = 0
     }
 
-    mutating func receive(input: Symbol) {
+    func receive(input: Symbol) {
         for transition in transitions {
-            if transition.origin == self.currentState && transition.input == input {
+            if transition.origin == self.currentState && transition.input == input && transition.condition(self) {
                 self.currentState = transition.destination
-                self.events[transition]?(transition)
+                transition.action(self)
                 break
             }
         }
-    }
-
-    mutating func addEvent(_ event: @escaping Event, to transition: Transition) {
-        self.events[transition] = event
     }
 
     func composeInParallel(with fsm: FiniteStateMachine) -> FiniteStateMachine {
@@ -103,6 +114,17 @@ struct FiniteStateMachine {
             }
         }
         return FiniteStateMachine(initialState: initialState, transitions: transitions)
+    }
+
+    func resetTimer() {
+        if internalTimer == nil {
+            self.internalTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(incrementTimeOneSecond), userInfo: nil, repeats: true)
+        }
+        self.timer = 0
+    }
+
+    @objc private func incrementTimeOneSecond() {
+        self.timer += 1
     }
 
 }
--- FiniteStateMachine/ViewController.swift
+++ FiniteStateMachine/ViewController.swift
@@ -12,41 +12,109 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let stateA1 = FiniteStateMachine.State(name: "A1")
-        let stateA2 = FiniteStateMachine.State(name: "A2")
-        let fsm1 = FiniteStateMachine(initialState: stateA1,
-                                      transitions: [FiniteStateMachine.Transition(origin: stateA1, input: "a", destination: stateA2),
-                                                    FiniteStateMachine.Transition(origin: stateA2, input: "c", destination: stateA2),
-                                                    FiniteStateMachine.Transition(origin: stateA2, input: "b", destination: stateA1)])
-
-        let stateB1 = FiniteStateMachine.State(name: "B1")
-        let stateB2 = FiniteStateMachine.State(name: "B2")
-        let stateB3 = FiniteStateMachine.State(name: "B3")
-        let fsm2 = FiniteStateMachine(initialState: stateB1,
-                                      transitions: [FiniteStateMachine.Transition(origin: stateB1, input: "a", destination: stateB2),
-                                                    FiniteStateMachine.Transition(origin: stateB2, input: "d", destination: stateB3),
-                                                    FiniteStateMachine.Transition(origin: stateB3, input: "b", destination: stateB1)])
-
-        var composedFsm = fsm1.composeInParallel(with: fsm2)
-
-        print (composedFsm.currentState)
-        composedFsm.receive(input: "a")
-        print (composedFsm.currentState)
-        composedFsm.receive(input: "a")
-        print (composedFsm.currentState)
-        composedFsm.receive(input: "a")
-        print (composedFsm.currentState)
-        composedFsm.receive(input: "a")
-        print (composedFsm.currentState)
-        composedFsm.receive(input: "b")
-        print (composedFsm.currentState)
-        composedFsm.receive(input: "b")
-        print (composedFsm.currentState)
         
-//        fsm1.addEvent({ _ in
-//            print ("Bla bla")
-//        }, to: FiniteStateMachine.Transition(origin: state1, input: .a, destination: state2))
+        let empty = FiniteStateMachine.State(name: "empty")
+        let full = FiniteStateMachine.State(name: "full")
+        let transition1 = FiniteStateMachine.Transition(origin: empty,
+                                                        input: "ingresa persona",
+                                                        destination: full,
+                                                        action: { fsm in fsm.variables["counter"] = (fsm.variables["counter"] as! Int) + 1 })
+        let transition2 = FiniteStateMachine.Transition(origin: full,
+                                                        input: "ingresa persona",
+                                                        destination: full,
+                                                        action: { fsm in fsm.variables["counter"] = (fsm.variables["counter"] as! Int) + 1 })
+        let transition3 = FiniteStateMachine.Transition(origin: full,
+                                                        input: "egresa persona",
+                                                        destination: full,
+                                                        condition: { fsm in (fsm.variables["counter"] as! Int) > 1 },
+                                                        action: { fsm in fsm.variables["counter"] = (fsm.variables["counter"] as! Int) - 1 })
+        let transition4 = FiniteStateMachine.Transition(origin: full,
+                                                        input: "egresa persona",
+                                                        destination: empty,
+                                                        condition: { fsm in (fsm.variables["counter"] as! Int) == 1 },
+                                                        action: { fsm in fsm.variables["counter"] = (fsm.variables["counter"] as! Int) - 1 })
+        let store = FiniteStateMachine(initialState: empty, transitions: [transition1, transition2, transition3, transition4], variables: ["counter": 0])
+
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "ingresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "ingresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "ingresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "ingresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "ingresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "egresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "egresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "egresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "egresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "egresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+        store.receive(input: "egresa persona")
+        print ("\(store.currentState) + \(String(describing: store.variables["counter"]))")
+
+//        let bluetoothOn = FiniteStateMachine.State(name: "Bluetooth On")
+//        let bluetoothOff = FiniteStateMachine.State(name: "Bluetooth Off")
+//        let fsmBluetooth = FiniteStateMachine(initialState: bluetoothOn,
+//                                              transitions: [FiniteStateMachine.Transition(origin: bluetoothOff, input: "turn on bluetooth", destination: bluetoothOn),
+//                                                            FiniteStateMachine.Transition(origin: bluetoothOn, input: "turn off bluetooth", destination: bluetoothOff),
+//                                                            FiniteStateMachine.Transition(origin: bluetoothOn, input: "connect", destination: bluetoothOn),
+//                                                            FiniteStateMachine.Transition(origin: bluetoothOn, input: "remove card", destination: bluetoothOn),
+//                                                            FiniteStateMachine.Transition(origin: bluetoothOn, input: "unsuspend", destination: bluetoothOn),
+//                                                            FiniteStateMachine.Transition(origin: bluetoothOn, input: "add card", destination: bluetoothOn),
+//                                                            FiniteStateMachine.Transition(origin: bluetoothOn, input: "suspend", destination: bluetoothOn)])
+//
+//        let noCards = FiniteStateMachine.State(name: "No Cards")
+//        let cardsAdded = FiniteStateMachine.State(name: "Cards Added")
+//        let cardsSuspended = FiniteStateMachine.State(name: "Suspended")
+//        let fsmCards = FiniteStateMachine(initialState: noCards,
+//                                          transitions: [FiniteStateMachine.Transition(origin: noCards, input: "add card", destination: cardsAdded),
+//                                                        FiniteStateMachine.Transition(origin: cardsAdded, input: "remove card", destination: noCards),
+//                                                        FiniteStateMachine.Transition(origin: cardsAdded, input: "suspend", destination: cardsSuspended),
+//                                                        FiniteStateMachine.Transition(origin: cardsSuspended, input: "unsuspend", destination: cardsAdded)])
+//
+//        let noDevice = FiniteStateMachine.State(name: "No Device")
+//        let connecting = FiniteStateMachine.State(name: "Connecting")
+//        let connected = FiniteStateMachine.State(name: "Connected")
+//        let fsmTPD = FiniteStateMachine(initialState: noDevice,
+//                                        transitions: [FiniteStateMachine.Transition(origin: noDevice, input: "connect", destination: connecting),
+//                                                      FiniteStateMachine.Transition(origin: connecting, input: "connect", destination: connected),
+//                                                      FiniteStateMachine.Transition(origin: connected, input: "add card", destination: connected),
+//                                                      FiniteStateMachine.Transition(origin: connected, input: "remove card", destination: connected),
+//                                                      FiniteStateMachine.Transition(origin: connected, input: "disconnect", destination: noDevice),
+//                                                      FiniteStateMachine.Transition(origin: connecting, input: "disconnect", destination: noDevice),
+//                                                      FiniteStateMachine.Transition(origin: connecting, input: "turn off bluetooth", destination: noDevice),
+//                                                      FiniteStateMachine.Transition(origin: connected, input: "turn off bluetooth", destination: noDevice),
+//                                                      FiniteStateMachine.Transition(origin: noDevice, input: "turn off bluetooth", destination: noDevice),
+//                                                      FiniteStateMachine.Transition(origin: connected, input: "suspend", destination: connected),
+//                                                      FiniteStateMachine.Transition(origin: connected, input: "unsuspend", destination: connected)])
+//
+//        var composedFsm = fsmTPD.composeInParallel(with: fsmBluetooth).composeInParallel(with: fsmCards)
+//
+//        print(composedFsm.currentState)
+//        composedFsm.receive(input: "turn off bluetooth")
+//        print(composedFsm.currentState)
+//        composedFsm.receive(input: "turn on bluetooth")
+//        print(composedFsm.currentState)
+//        composedFsm.receive(input: "connect")
+//        print(composedFsm.currentState)
+//        composedFsm.receive(input: "connect")
+//        print(composedFsm.currentState)
+//        composedFsm.receive(input: "remove card")
+//        print(composedFsm.currentState)
+//        composedFsm.receive(input: "add card")
+//        print(composedFsm.currentState)
+//        composedFsm.receive(input: "turn off bluetooth")
+//        print(composedFsm.currentState)
+//        composedFsm.receive(input: "turn on bluetooth")
+        
+        
 
     }
 
